70484f406f42b9ab5098b76da8324be2d9e6e7cf
Remus Lazar <rl@cron.eu>
2015-02-13 17:02:47 +0100

    install php-soap and tweak php execution time

--- container-files/build-typo3-app/pre-install-typo3-app.sh
+++ container-files/build-typo3-app/pre-install-typo3-app.sh
@@ -5,7 +5,7 @@
 # into archived package in /tmp directory.
 #
 # This archive will be then used when container starts for the 1st time.
-# We do that to avoid installing TYPO3 app during the runtime, which 
+# We do that to avoid installing TYPO3 app during the runtime, which
 # can be slow and potentially error-prone (i.e. composer conflicts/timeouts etc).
 #
 
@@ -14,6 +14,15 @@ set -u
 
 # Needs to be absolute as user can call this script from Dockerfile in multiple ways...
 source /build-typo3-app/include-variables.sh
+
+echo
+echo "Configure PHP for TYPO3 CMS"
+echo
+
+# install needed php-modules
+yum install -y php-soap
+# tweak the php max_execution_time
+sed -i 's/^max_execution_time = 120/max_execution_time = 240/' /etc/php.d/zz-php.ini
 
 echo
 echo "Installing TYPO3 app from $T3APP_BUILD_REPO_URL ($T3APP_BUILD_BRANCH) repository..."
@@ -35,7 +44,7 @@ git log -10 --pretty=format:"%h %an %cr: %s" --graph
 COMPOSER_PROCESS_TIMEOUT=900 composer install $T3APP_BUILD_COMPOSER_PARAMS
 
 # If the project contains executable build.sh in the root directory
-# it will be run during 'docker build' process. Note: it's OK to run is as root 
+# it will be run during 'docker build' process. Note: it's OK to run is as root
 # as it might need these privileges to install some global tools.
 if [[ -x $T3APP_USER_BUILD_SCRIPT ]]; then $T3APP_USER_BUILD_SCRIPT --preinstall; fi
 
@@ -46,4 +55,4 @@ tar -zcf $INSTALLED_PACKAGE_NAME.tgz $INSTALLED_PACKAGE_NAME && rm -rf $INSTALLE
 echo
 echo "TYPO3 app from $T3APP_BUILD_REPO_URL ($T3APP_BUILD_BRANCH) installed."
 echo $(ls -lh $CWD)
-echo 
+echo
